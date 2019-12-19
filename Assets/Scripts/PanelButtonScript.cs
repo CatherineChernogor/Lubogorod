13a0af9e3848605ad8423b3d5fc7ffd297560b3f
CathCher <chernogor_k@mail.ru>
2019-12-19 19:44:39 +0800

    first working version

---
+++ Assets/Scripts/PanelButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//SPS SetPanelScript;
+public class PanelButtonScript : MonoBehaviour
+{
+    public int id;
+    public GameObject panel;
+    public SetPanelScript SetPanelScript;
+
+    public void closePanel()
+    {
+        print("close");
+        panel.SetActive(false);
+    }
+
+    public void goBackward()
+    {
+        print("backward");
+        id--;
+        SetPanelScript.openPanel(id);
+    }
+    public void goforward()
+    {
+        print("forward");
+        id++;
+        SetPanelScript.openPanel(id);
+
+        //openPanel(id + 1);
+    }
+}
