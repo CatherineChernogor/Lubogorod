d5a83e9fce2a5c22a3975383d97acf629f672231
CathCher <chernogor_k@mail.ru>
2019-12-20 19:07:45 +0800

    work without ar

---
+++ Assets/Scripts/ex.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ex : MonoBehaviour {
+
+    public GameObject img;
+        
+    
+    private void Start() {
+        
+    }
+
+    
+}
