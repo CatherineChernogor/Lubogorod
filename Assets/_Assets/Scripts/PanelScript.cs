b25637176d3b25ed34017afe8ac932165c50f039
CathCher <chernogor_k@mail.ru>
2020-02-04 00:27:10 +0800

    beta-version

---
+++ Assets/_Assets/Scripts/PanelScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        id = 0;
+    }
+    public static int id;
+    private const int low = 1;
+    private const int high = 8;
+    public GameObject panel;
+    public GameObject img;
+    public GameObject text; 
+
+    public void closePanel()
+    {
+        print("close");
+        panel.SetActive(false);
+        id = 0;
+    }
+    public void goBackward()
+    {
+        print("backward " + id + "->" + (id - 1));
+        id--;
+        if (id < low)
+            id = high;
+
+        openPanel(id);
+    }
+    public void goforward()
+    {
+        print("forward " + id + "->" + (id + 1));
+        id++;
+        if (id > high)
+            id = low;
+
+        openPanel(id);
+    }
+
+    string prof;
+    public void openPanel(int loc_id)
+    {
+        id = loc_id;
+        print("open" + loc_id);
+        if (!panel.activeSelf)
+            panel.SetActive(true);
+
+        img.GetComponent<Image>().sprite = Resources.Load<Sprite>("gif/face_" + id);
+        prof= Resources.Load<TextAsset>("text/pro_" + id).ToString();
+        text.GetComponent<Text>().text = prof;
+
+    }
+}
---
+++ Assets/_Assets/Scripts/PlanePressScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanePressScript : MonoBehaviour
+{
+    public int id;
+    public PanelScript panelScript;
+    private void OnMouseDown()
+    {
+        panelScript.openPanel(id);
+    }
+}
